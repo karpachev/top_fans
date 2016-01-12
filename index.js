9c7eb2ae91009e87937b6083579674b0b8a339cb
karpachev <v.karpachev@gmail.com>
2016-01-12 22:43:24 +0200

    get the whole feed

---
+++ index.js
@@ -0,0 +1,73 @@
+var FB = require("fb");
+var request = require("request");
+var fs = require("fs");
+
+
+var ACCESS_TOKEN = readFileSync("access_token.txt");
+
+
+
+var feed = [];
+
+FB.setAccessToken(ACCESS_TOKEN);
+
+FB.api(
+	"/komfo.bg/feed"
+	,{
+			fields: 'message,story,description,created_time,from' + 
+					',shares' +
+					',likes.summary(true).limit(100).order(reverse_chronological){name}' +
+					',comments.summary(true).order(reverse_chronological).limit(100){from,message,likes.summary(true).limit(100).order(reverse_chronological){name}}'
+			,limit: 100
+	}
+	,function(res) {
+		if(!res || res.error) {
+			console.log(!res ? 'error occurred' : res.error);
+			return;
+		}
+
+	    if (res.data) {
+	    	process_posts(res.data);
+	    }
+
+		feed_history(res.paging.next);
+	}
+);
+
+function feed_history(next_url) 
+{
+	request(
+		next_url,
+		function (error, response, res) {
+			if (!error && response.statusCode == 200) {
+				res = JSON.parse(res);
+			    // console.dir(res);
+			    if (res.data) {
+			    	process_posts(res.data);
+			    }
+
+
+				if (res.paging && res.paging.next) {
+					feed_history(res.paging.next);
+				} else {
+					extract_comments();
+				}
+			}
+		}
+	);
+}
+
+function process_posts(posts)
+{
+	posts.forEach(function(v,i,arr){
+		process.stdout.write(".");
+		feed.push(v);
+	});
+
+	console.log("Stored %d posts", posts.length);
+}
+
+function extract_comments()
+{
+	console.log("Extracting comments");
+}
